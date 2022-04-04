547134fea8c5ff751ddbc4cd0f9676c9c9ac64d9
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-04 12:40:56 +0530

    1 charactersation Test Added

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -6,13 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void shouldCheckIfCustomerStatementIsCorrect(){
-        Customer customer=new Customer("Sanjay");
-        customer.addRental(new Rental(new Movie("RRR",Movie.REGULAR),2));
+    public void shouldCheckIfCustomerStatementIsCorrect() {
+        Customer customer = new Customer("Sanjay");
+        customer.addRental(new Rental(new Movie("Kashmiri Files", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Tom and Jerry", Movie.CHILDRENS), 4));
+        customer.addRental(new Rental(new Movie("RRR", Movie.NEW_RELEASE), 2));
         assertEquals("Rental Record for Sanjay\n" +
-                "\tRRR\t2.0\n" +
-                "Amount owed is 2.0\n" +
-                "You earned 1 frequent renter points",customer.statement());
+                "\tKashmiri Files\t3.5\n" +
+                "\tTom and Jerry\t3.0\n" +
+                "\tRRR\t6.0\n" +
+                "Amount owed is 12.5\n" +
+                "You earned 4 frequent renter points", customer.statement());
 
     }
 
